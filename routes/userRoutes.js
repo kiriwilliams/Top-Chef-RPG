a605c706b3c76ffe79753ac6e2cc06547a65ef0f
sharky <zeebing@gmail.com>
2019-05-10 12:04:15 -0400

    commented out the old app.get login info

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,53 +1,87 @@
 var db = require("../models");
+const md5 = require("md5");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // add a new user if none exists
   //returns false if username is already taken
-  app.post("/api/users", function(req, res) {
+  app.post("/api/users", function (req, res) {
     db.users.findOrCreate({
-      where:{
+      where: {
         username: req.body.username,
       },
     }).then(([user, created]) => {
-      if (created){
-        console.log(req.body);
+      if (created) {
         db.users.update(
-          {password: req.body.password},
-          {where: {username: req.body.username}},
-        ).then(function(results){
-          console.log(results);
+          { password: md5(req.body.password) },
+          { where: { username: req.body.username } },
+        ).then(function (results) {
+          console.log("findorcreate: "+results);
+
         });
-    }
+      }
       res.send(created);
     });
   });
 
-  //check a user login
-  app.get("/api/users/:username", function(req, res) {
-    var username = req.params.username;
-    var password = req.params.username;
-  
+  //check user login (POST)
+  app.post("/api/login", function (req, res) {
+    console.log("un: " + req.body.username);
+    console.log("pw: " + req.body.password);
+
+    var username = req.body.username;
+    var password = md5(req.body.password);
     db.users.findOne({
       where: {
         username: username
       }
-    }).then(function(result) {
-      if(!result){
+    }).then(function (result) {
+      console.log(result);
+      if (!result) {
         console.log("first send");
         return res.send("That username does not exist");
       }
       console.log(result.dataValues);
 
-      if(password === result.dataValues.password){
+      if (password === result.dataValues.password) {
         console.log("logged in");
         // res.render("game.handlebars");
       }
-      else{
+      else {
         console.log("bad password");
         return res.send("Username and password do not match");
       };
 
-    });
+    })
+
   });
+
+
+  //check a user login
+  // app.get("/api/users/:username/:password", function (req, res) {
+  //   var username = req.params.username;
+  //   var password = req.params.password;
+
+  //   db.users.findOne({
+  //     where: {
+  //       username: username
+  //     }
+  //   }).then(function (result) {
+  //     if (!result) {
+  //       console.log("first send");
+  //       return res.send("That username does not exist");
+  //     }
+  //     console.log(result.dataValues);
+
+  //     if (password === result.dataValues.password) {
+  //       console.log("logged in");
+  //       // res.render("game.handlebars");
+  //     }
+  //     else {
+  //       console.log("bad password");
+  //       return res.send("Username and password do not match");
+  //     };
+
+  //   });
+  // });
 
 };
