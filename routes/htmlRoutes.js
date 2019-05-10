7eb195b5157a0a031f2b16afd1c13bd1747894e3
sharky <zeebing@gmail.com>
2019-05-10 15:00:53 -0400

    removed obsolete character-select route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,26 +1,43 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load login page, does not interact with database
-  app.get("/", function(req, res) {
-      res.render("index");
-      console.log("index loaded");
+  app.get("/", function (req, res) {
+    res.render("index");
+    console.log("index loaded");
   });
 
   // Load info about country and NPCs and return it to handlebars page
-  app.get("/game/:id", function(req, res) {
+  app.get("/game/:id", function (req, res) {
     //country grab
-    db.Environment.findOne({       
+    db.Environment.findOne({
 
       where: {
         id: req.params.id
       },
       include: [db.Npc, db.Dishes]
 
-      }).then(function(result) {
-        var info = result.dataValues;
+    }).then(function (result) {
+      var info = result.dataValues;
       res.render("game", info);
       console.log(info);
     });
   });
+
+
+  app.get("/character-select/:userId", function (req, res) {
+    db.Character.findAll({
+      where: {
+        userId: req.params.userId
+      }
+    }).then(function (result) {
+      console.log(result);
+      var characters = result;
+      res.render("character-select", characters);
+
+    });
+
+  })
+
 };
+
