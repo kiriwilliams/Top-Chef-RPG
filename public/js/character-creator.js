9b88a062d2ad1c176e295f3fb368d4b04f9c1c12
snarkishark <snarkishark@gmail.com>
2019-05-13 12:00:10 -0400

    post /api/createCharacter fn then window.location.reload

--- public/js/character-creator.js
+++ public/js/character-creator.js
@@ -1,8 +1,36 @@
+
 $(document).ready(function(){
     $("#newCharacter").on("click", function(e){
         e.preventDefault();
         $('#characterCreator').modal('show')
     });
+
+    $("#createCharacter").on("click", function(e){
+        e.preventDefault();
+        var name = $("#character-name").val().trim();
+        var roast = $("#skill_R").val().trim();
+        var knife = $("#skill_K").val().trim();
+        var grill = $("#skill_G").val().trim();
+        var sauce = $("#skill_S").val().trim();
+        var dough = $("#skill_D").val().trim();
+        var userID = window.sessionStorage.getItem("userID");
+
+        $.post("/api/createCharacter",{
+            name: name,
+            skill_R: roast,
+            skill_K: knife,
+            skill_G: grill,
+            skill_S: sauce,
+            skill_D: dough,
+            UserId: userID
+
+        }).then(function(result){
+            console.log("createCharacter api call: "+result);
+            window.location.reload();
+        });
+        
+
+    })
 
    
 }); //close document ready
