f4f2097cbeecc154a2edac4d20732d7626bfc826
Will M <wmcna97@gmail.com>
2019-05-13 12:00:53 -0400

    added stats view to home page

--- public/js/home.js
+++ public/js/home.js
@@ -12,7 +12,16 @@ $(document).ready(function(){
         e.preventDefault();
         //logout function
     });
-    
+
     //puts the character name at top of page
     $("#charName").text(sessionStorage.getItem("character"))
+
+    //get char stats
+    $.get("/api/character/" + sessionStorage.getItem("character"), function(result){
+        $("#skill_R").text(result.skill_R);
+        $("#skill_K").text(result.skill_K);
+        $("#skill_G").text(result.skill_G);
+        $("#skill_S").text(result.skill_S);
+        $("#skill_D").text(result.skill_D);
+    });
 });
