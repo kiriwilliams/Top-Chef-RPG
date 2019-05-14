1a2cfab11e619a38f51dc1db14e1129a46c695a9
SGeorges <SGeorges@github.com>
2019-05-14 11:25:45 -0400

    Shokugekei no Gordon

--- database_info/seeds.sql
+++ database_info/seeds.sql
@@ -22,19 +22,19 @@ INSERT INTO npcs (chef_name, score_threshold, npc_img, EnvironmentId)
 VALUES ('Rafael', 10, 'http://clipart-library.com/img/1501586.gif', 1);
 
 INSERT INTO npcs (chef_name, score_threshold, npc_img, EnvironmentId)
-VALUES ('Julia', 10, 'https://i.pinimg.com/originals/10/15/6d/10156d7f767f81df76fb5a6c978579b1.jpg', 2);
+VALUES ('Julia', 40, 'https://i.pinimg.com/originals/10/15/6d/10156d7f767f81df76fb5a6c978579b1.jpg', 2);
 
 INSERT INTO npcs (chef_name, score_threshold, npc_img, EnvironmentId)
-VALUES ('Hiroyuki', 10, 'https://media.istockphoto.com/vectors/japanese-cook-with-sushi-traditional-food-profession-vector-id939201282?k=6&m=939201282&s=612x612&w=0&h=Kk-Xg1b8nVBNLIio40C_tOZ5jWAACQI7MhcbQ0zid7g=', 3);
+VALUES ('Hiroyuki', 20, 'https://media.istockphoto.com/vectors/japanese-cook-with-sushi-traditional-food-profession-vector-id939201282?k=6&m=939201282&s=612x612&w=0&h=Kk-Xg1b8nVBNLIio40C_tOZ5jWAACQI7MhcbQ0zid7g=', 3);
 
 INSERT INTO npcs (chef_name, score_threshold, npc_img, EnvironmentId)
-VALUES ('Chan', 10, 'https://clipground.com/images/asian-cuisine-clipart-13.jpg', 4);
+VALUES ('Chan', 16, 'https://clipground.com/images/asian-cuisine-clipart-13.jpg', 4);
 
 INSERT INTO npcs (chef_name, score_threshold, npc_img, EnvironmentId)
-VALUES ('Amara', 10, 'https://bestill.nabomat.no/system/images/bjNHrspWWr54aabagV08Sb/medium/000a313c63e8a8446ddcf396a696ca8b_indian-woman-with-indian-food-indian-lady-chef-clipart_1500-1600.jpeg?1495478494', 5);
+VALUES ('Amara', 12, 'https://bestill.nabomat.no/system/images/bjNHrspWWr54aabagV08Sb/medium/000a313c63e8a8446ddcf396a696ca8b_indian-woman-with-indian-food-indian-lady-chef-clipart_1500-1600.jpeg?1495478494', 5);
 
 INSERT INTO npcs (chef_name, score_threshold, npc_img, EnvironmentId)
-VALUES ('Gordon', 10, 'https://render.fineartamerica.com/images/rendered/default/greeting-card/images-medium-5/gordon-ramsay-art.jpg?&targetx=-21&targety=0&imagewidth=542&imageheight=700&modelwidth=500&modelheight=700&backgroundcolor=FAFCFB&orientation=1', 6);
+VALUES ('Gordon', 80, 'https://render.fineartamerica.com/images/rendered/default/greeting-card/images-medium-5/gordon-ramsay-art.jpg?&targetx=-21&targety=0&imagewidth=542&imageheight=700&modelwidth=500&modelheight=700&backgroundcolor=FAFCFB&orientation=1', 6);
 
 -- dishes seeds (Italian)
 INSERT INTO dishes (name, skill, mod_R, mod_K, mod_G, mod_S, mod_D, EnvironmentId)
@@ -87,11 +87,11 @@ INSERT INTO dishes (name, skill, mod_R, mod_K, mod_G, mod_S, mod_D, EnvironmentI
 VALUES ('Tandoori Chicken', 'Roasting', 1, 0, 0, 0, 0, 5);
 
 -- dishes seeds (Gordon)
-INSERT INTO dishes (name, mod_F, mod_J, mod_T, mod_c, mod_I, EnvironmentId)
-VALUES ('Omelette', 0.0, 0.0, 0.0, 0.0, 0.0, 6);
+INSERT INTO dishes (name, skill, mod_R, mod_K, mod_G, mod_S, mod_D, EnvironmentId)
+VALUES ('Autumn Pacific Saury Cartoccio', 'Unknown', 0.4, 0.3, 0.1, 0.2, 0, 6);
 
-INSERT INTO dishes (name, mod_F, mod_J, mod_T, mod_c, mod_I, EnvironmentId)
-VALUES ('Pork Tenderloin', 0.0, 0.0, 0.0, 0.0, 0.0, 6);
+INSERT INTO dishes (name, skill, mod_R, mod_K, mod_G, mod_S, mod_D, EnvironmentId)
+VALUES ('Sauce Chevreuil', 'Unknown', 0.2, 0.3, 0, 0.5, 0, 6);
 
-INSERT INTO dishes (name, mod_F, mod_J, mod_T, mod_c, mod_I, EnvironmentId)
-VALUES ('Calamari', 0.0, 0.0, 0.0, 0.0, 0.0, 6);
+INSERT INTO dishes (name, skill, mod_R, mod_K, mod_G, mod_S, mod_D, EnvironmentId)
+VALUES ('Time Fuse Mapo Curry Noodles', 'Unknown', 0, 0.1, 0.1, 0.1, 0.7, 6);
