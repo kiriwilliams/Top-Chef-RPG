61eefab7681cb9e9cf13b1bf04205197f2043508
sharky <zeebing@gmail.com>
2019-05-14 21:28:39 -0400

    removed unused commented out code

--- public/js/index.js
+++ public/js/index.js
@@ -4,55 +4,6 @@ var password = $("#password");
 
 
 
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-// var handleFormSubmit = function (type) {
-//   // event.preventDefault();
-//   console.log(type);
-
-//   var user = {
-//     username: username.val().trim(),
-//     password: password.val().trim()
-//   };
-//   console.log(user);
-
-//   // if (!(user.username && user.password)) {
-//   //   alert("You must enter an example text and description!");
-//   //   return;
-//   // }
-
-//   if (type === "log-in") {
-//     API.loginUser(user).then(function () {
-//       $.get("/api/users", {
-//         username: username,
-//         password: password
-//       }).then(function (data) {
-//         //do something
-//       });
-
-//     });
-//   }
-//   else if (type === "sign-up") {
-//     $.post("/api/users", {
-//       username: username,
-//       password: password
-//     }).then(function (data) {
-//       console.log(data);
-//       //go to game page
-
-//     })
-//   }
-
-// }
-// var form = $(".needs-validation");
-// $(form).on("submit", function(e){
-//   if(form.checkValidity() === false){
-//     e.preventDefault();
-//     e.stopPropagation();
-//   }
-//   form.addClass("was-validated");
-// }, false);
-
 function logIn(userId) {
   window.sessionStorage.setItem("userId", userId);
   window.location.replace("/character-select/" + userId);
@@ -117,17 +68,6 @@ $("#sign-up-submit").on("click", function (e) {
     }
     return;
   }
-
-  // $.ajax("/api/users", {
-  //   type: "POST",
-  //   data: {
-  //     username: $("#username").val().trim(),
-  //     password: $("#password").val().trim()
-  //   }
-  // }).then(function (data) {
-  //   console.log("the data is " + data);
-  //   badUsername();
-  // })
 
   createAccount(username, password);
 
